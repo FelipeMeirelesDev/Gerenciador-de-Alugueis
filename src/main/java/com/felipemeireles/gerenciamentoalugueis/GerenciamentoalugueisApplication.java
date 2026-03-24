b1da07eae16fb07044cd32f4065fd3050e082524
FelipeMeirelesDev <felipegoncalvesmeireles2003@gmail.com>
2026-03-24 08:38:53 -0300

    chore: initial commit - projeto spring boot criado

---
+++ src/main/java/com/felipemeireles/gerenciamentoalugueis/GerenciamentoalugueisApplication.java
@@ -0,0 +1,13 @@
+package com.felipemeireles.gerenciamentoalugueis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoalugueisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoalugueisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipemeireles/gerenciamentoalugueis/GerenciamentoalugueisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipemeireles.gerenciamentoalugueis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoalugueisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
